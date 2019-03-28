5cce9c866eb49964fe40bfc42563950439de5981
Stefan Heinen <heini.phys.chem@gmail.com>
2019-03-28 17:30:35 +0100

    new dict

--- get_inchis.py
+++ get_inchis.py
@@ -6,11 +6,12 @@ lines = open(sys.argv[1], 'r').readlines()
 data = {}
 
 for i,line in enumerate(lines):
+    name = (line[:-1].split(',')[0])
     inchi = (line[:-1].split()[-1])[1:-3]
     energy = (line[:-1].split()[1])[1:-2]
 
-    data[inchi] = energy
+    data[name] = (inchi, energy)
 
 for key, value in data.items():
-    print(key, value)
+    print(key.split('_')[1], value[0])
 
